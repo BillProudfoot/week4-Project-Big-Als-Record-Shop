3e562e3e8d12c008b151fa9b7ebc4b05a867d83e
Bill Proudfoot <user@CODECLAN031.local>
2017-04-04 09:28:54 +0100

    Added TDD on only calculation mark up

---
+++ specs/album_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../album.rb')
+
+class AlbumTest < MiniTest::Test
+
+
+  def setup()
+    @sell_price = 10
+    @buy_price = 5
+  end
+
+  def test_mark_up 
+    mark_up = ((sell_price/buy_price) - 1) * 100
+    assert_equal(mark_up, 100)
+  end
+
+
+end
+
+
+
